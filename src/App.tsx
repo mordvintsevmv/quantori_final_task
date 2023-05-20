357520896c30e2e0db07c558d2e3357c8eca5778
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-20 23:26:45 +0400

    Feature: implementing auth listener from Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
 import "./App.css"
 
+import { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
 
+import { auth } from "./api/firebase.ts"
+import { useTypedDispatch } from "./hooks/reduxHooks.ts"
+import Auth from "./pages/Auth/Auth.tsx"
 import Main from "./pages/Main/Main.tsx"
+import { login, logout } from "./redux/slices/authSlice.ts"
 
 const App = () => {
+  const dispatch = useTypedDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            emailVerified: user.emailVerified,
+            isAnonymous: user.isAnonymous,
+            phoneNumber: user.phoneNumber,
+            photoURL: user.photoURL,
+          }),
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    return () => unsubscribe()
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   )
 }
