15275645556c8c3e23f7588e0f32d5c8317365be
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-05-25 19:41:22 +0400

    Feature: implementing Redirecting for logged / not logged users

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import "./App.css"
 
-import { useEffect } from "react"
+import { Fragment, useEffect } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { onAuthStateChanged } from "firebase/auth"
 
 import { auth } from "./api/firebase.ts"
-import { useTypedDispatch } from "./hooks/reduxHooks.ts"
+import Loading from "./components/Loading/Loading.tsx"
+import { useTypedDispatch, useTypedSelector } from "./hooks/reduxHooks.ts"
 import Auth from "./pages/Auth/Auth.tsx"
 import Main from "./pages/Main/Main.tsx"
 import NotFound from "./pages/NotFound/NotFound.tsx"
 import ProteinPage from "./pages/Protein/ProteinPage.tsx"
 import Search from "./pages/Search/Search.tsx"
 import { login, logout } from "./redux/slices/authSlice.ts"
+import { statusType } from "./types/statusType.ts"
 
 const App = () => {
   const dispatch = useTypedDispatch()
+  const { user, authStatus } = useTypedSelector((state) => state.auth)
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
         dispatch(
           login({
-            uid: user.uid,
-            email: user.email,
-            displayName: user.displayName,
-            emailVerified: user.emailVerified,
-            isAnonymous: user.isAnonymous,
-            phoneNumber: user.phoneNumber,
-            photoURL: user.photoURL,
+            uid: authUser.uid,
+            email: authUser.email,
+            displayName: authUser.displayName,
+            emailVerified: authUser.emailVerified,
+            isAnonymous: authUser.isAnonymous,
+            phoneNumber: authUser.phoneNumber,
+            photoURL: authUser.photoURL,
           }),
         )
       } else {
@@ -39,14 +42,31 @@ const App = () => {
   }, [dispatch])
 
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/protein/:id" element={<ProteinPage />} />
-      <Route path="/not-found" element={<NotFound />} />
-      <Route path="*" element={<Navigate to="/not-found" />} />
-    </Routes>
+    <Fragment>
+      {authStatus === statusType.SUCCESS && (
+        <Routes>
+          <Route
+            path="/"
+            element={user ? <Navigate to="/search" /> : <Main />}
+          />
+          <Route
+            path="/auth"
+            element={user ? <Navigate to="/search" /> : <Auth />}
+          />
+          <Route
+            path="/search"
+            element={user ? <Search /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/protein/:id"
+            element={user ? <ProteinPage /> : <Navigate to="/auth" />}
+          />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" />} />
+        </Routes>
+      )}
+      {authStatus === statusType.IDLE && <Loading />}
+    </Fragment>
   )
 }
 
